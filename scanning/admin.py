b09ef231f99044bf071c2937f0d640234071f194
Nickhodem <nickhodem@gmail.com>
2015-06-28 19:40:49 +0200

    after crushed

--- scanning/admin.py
+++ scanning/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
 from scanning.models import Category, Page, Tachymeter
 
 class PageAdmin(admin.ModelAdmin):
@@ -9,9 +8,3 @@ class PageAdmin(admin.ModelAdmin):
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
 admin.site.register(Tachymeter, PageAdmin)
-=======
-from scanning.models import Category, Page
-
-admin.site.register(Category)
-admin.site.register(Page)
->>>>>>> 26b00b2f6542fe123b9fd5420c36e72e3c5dc52d
--- scanning/models.py
+++ scanning/models.py
@@ -1,18 +1,40 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
     def __unicode__(self):
         return self.name
+'''
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+'''
+
 
 class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    url = models.URLField()
+    quality = models.PositiveSmallIntegerField(default=1)
+    views = models.IntegerField(default=0)
+    likes=models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+class Tachymeter(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     name =models.CharField(max_length=128)
     url = models.URLField()
     quality = models.PositiveSmallIntegerField(default=1)
     views = models.IntegerField(default=0)
+    likes=models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
