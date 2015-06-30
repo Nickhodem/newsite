6876eb2ae168e3876857b454c6a6edcf6a91dd21
Nickhodem <nickhodem@gmail.com>
2015-06-30 21:47:13 +0200

    preview of resume

--- scanning/urls.py
+++ scanning/urls.py
@@ -4,4 +4,5 @@ from scanning import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^about/', views.about, name='about'),
+        url(r'^add_category/$', views.add_category, name='add_category'),
         url(r'^scanners/', views.scanners, name='scanners'),)
